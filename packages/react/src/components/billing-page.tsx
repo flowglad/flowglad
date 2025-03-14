5131ea4c218f4a9f8a9709432f268bcab0c7392a
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-14 10:53:00 -0400

    lock workspace

--- packages/react/src/components/billing-page.tsx
+++ packages/react/src/components/billing-page.tsx
@@ -3,6 +3,7 @@
 import { Flowglad } from '@flowglad/node'
 import { Invoices } from './invoices'
 import { cn } from '../lib/utils'
+import { PaymentMethods } from './payment-methods'
 
 export function BillingPage({
   billing,
@@ -13,6 +14,8 @@ export function BillingPage({
 }) {
   return (
     <div className={cn('flex flex-col gap-4 p-4', className)}>
+      <h3 className="text-2xl font-bold">Payment Methods</h3>
+      <PaymentMethods paymentMethods={billing.paymentMethods} />
       <h3 className="text-2xl font-bold">Invoices</h3>
       <Invoices invoices={[]} />
     </div>
