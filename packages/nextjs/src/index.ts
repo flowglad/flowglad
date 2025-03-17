cfaa4d8f68560dd6758fedf3417bc2ea91bf525c
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-17 12:07:19 -0400

    export pricing table

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -7,6 +7,7 @@ import {
   FlowgladProvider,
   useBilling,
   BillingPage,
+  PricingTable,
 } from '@flowglad/react'
 
-export { FlowgladProvider, useBilling, BillingPage }
+export { FlowgladProvider, useBilling, BillingPage, PricingTable }
