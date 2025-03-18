7a8b7ef5f5cd996f2e4062aadcaafd8b9f8848b9
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-18 12:08:06 -0400

    migrations generated but failing

---
+++ platform/flowglad-next/drizzle-migrations/0170_classy_wiccan.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "customer_profiles" DROP CONSTRAINT "customer_profiles_customer_id_customers_id_fk";
+--> statement-breakpoint
+DROP INDEX IF EXISTS "customer_profiles_customer_id_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "customer_profiles_customer_id_organization_id_unique_idx";--> statement-breakpoint
+ALTER TABLE "customer_profiles" ALTER COLUMN "name" SET NOT NULL;--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "customer_profiles_organization_id_email_unique_idx" ON "customer_profiles" USING btree ("organization_id","email");--> statement-breakpoint
+ALTER TABLE "customer_profiles" DROP COLUMN IF EXISTS "customer_id";
