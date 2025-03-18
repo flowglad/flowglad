e0e836799aa167b6ac0806688dfd6cf54023c712
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-18 12:33:18 -0400

    migration succeeding

--- platform/flowglad-next/drizzle-migrations/0170_classy_wiccan.sql
+++ platform/flowglad-next/drizzle-migrations/0170_classy_wiccan.sql
@@ -4,4 +4,5 @@ DROP INDEX IF EXISTS "customer_profiles_customer_id_idx";--> statement-breakpoin
 DROP INDEX IF EXISTS "customer_profiles_customer_id_organization_id_unique_idx";--> statement-breakpoint
 ALTER TABLE "customer_profiles" ALTER COLUMN "name" SET NOT NULL;--> statement-breakpoint
 CREATE UNIQUE INDEX IF NOT EXISTS "customer_profiles_organization_id_email_unique_idx" ON "customer_profiles" USING btree ("organization_id","email");--> statement-breakpoint
+DROP POLICY IF EXISTS "Enable read access via CustomerProfiles" ON "customers";
 ALTER TABLE "customer_profiles" DROP COLUMN IF EXISTS "customer_id";
