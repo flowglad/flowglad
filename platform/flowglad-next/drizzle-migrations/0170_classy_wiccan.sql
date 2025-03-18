350f473e93c176d7139325fc97641c686e6e33eb
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-18 13:32:16 -0400

    fix name not null migration

--- platform/flowglad-next/drizzle-migrations/0170_classy_wiccan.sql
+++ platform/flowglad-next/drizzle-migrations/0170_classy_wiccan.sql
@@ -2,6 +2,7 @@ ALTER TABLE "customer_profiles" DROP CONSTRAINT "customer_profiles_customer_id_c
 --> statement-breakpoint
 DROP INDEX IF EXISTS "customer_profiles_customer_id_idx";--> statement-breakpoint
 DROP INDEX IF EXISTS "customer_profiles_customer_id_organization_id_unique_idx";--> statement-breakpoint
+UPDATE "customer_profiles" SET "name" = "email" where name is null;--> statement-breakpoint
 ALTER TABLE "customer_profiles" ALTER COLUMN "name" SET NOT NULL;--> statement-breakpoint
 CREATE UNIQUE INDEX IF NOT EXISTS "customer_profiles_organization_id_email_unique_idx" ON "customer_profiles" USING btree ("organization_id","email");--> statement-breakpoint
 DROP POLICY IF EXISTS "Enable read access via CustomerProfiles" ON "customers";
