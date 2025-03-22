37b737e07ee224b9134e71cef856528b3adbbb58
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-22 11:40:23 -0400

    catlaog and mutation

--- platform/flowglad-next/src/server/mutations/editCatalog.ts
+++ platform/flowglad-next/src/server/mutations/editCatalog.ts
@@ -1,14 +1,24 @@
 import { protectedProcedure } from '@/server/trpc'
 import { authenticatedTransaction } from '@/db/databaseMethods'
 import { editCatalogSchema } from '@/db/schema/catalogs'
-import { updateCatalog } from '@/db/tableMethods/catalogMethods'
+import {
+  makeCatalogDefault,
+  updateCatalog,
+} from '@/db/tableMethods/catalogMethods'
 
 export const editCatalog = protectedProcedure
   .input(editCatalogSchema)
   .mutation(async ({ input }) => {
     const catalog = await authenticatedTransaction(
       async ({ transaction }) => {
-        return updateCatalog(input.catalog, transaction)
+        const catalog = await updateCatalog(
+          input.catalog,
+          transaction
+        )
+        if (catalog.isDefault) {
+          return makeCatalogDefault(catalog, transaction)
+        }
+        return catalog
       }
     )
     return {
