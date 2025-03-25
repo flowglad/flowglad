48eb1cc706f461f78b3b80e41291b360f273c04e
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-25 11:13:56 -0400

    fix lint

--- platform/flowglad-next/src/stubs/organizationStubs.ts
+++ platform/flowglad-next/src/stubs/organizationStubs.ts
@@ -20,5 +20,6 @@ export const dummyOrganization: Organization.Record = {
   defaultCurrency: CurrencyCode.USD,
   billingAddress: null,
   contactEmail: null,
+  featureFlags: {},
   allowMultipleSubscriptionsPerCustomer: false,
 }
