a01e86a3023a292728914842a00d6bc7b4a4750f
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-26 15:23:53 -0400

    disabled style passthrough

--- packages/react/src/components/ui/button.tsx
+++ packages/react/src/components/ui/button.tsx
@@ -8,6 +8,9 @@ const buttonVariants = cva(
   "flowglad-inline-flex flowglad-items-center flowglad-justify-center flowglad-gap-2 flowglad-whitespace-nowrap flowglad-rounded-md flowglad-text-sm flowglad-font-medium flowglad-transition-[color,box-shadow] flowglad-disabled:pointer-events-none flowglad-disabled:opacity-50 [&_svg]:flowglad-pointer-events-none [&_svg:not([class*='size-'])]:flowglad-size-4 flowglad-shrink-0 [&_svg]:flowglad-shrink-0 flowglad-outline-none flowglad-focus-visible:border-ring flowglad-focus-visible:ring-ring/50 flowglad-focus-visible:ring-[3px] flowglad-aria-invalid:ring-destructive/20 flowglad-dark:aria-invalid:ring-destructive/40 flowglad-aria-invalid:border-destructive hover:flowglad-cursor-pointer",
   {
     variants: {
+      disabled: {
+        true: 'flowglad-opacity-50',
+      },
       variant: {
         default:
           'flowglad-bg-primary flowglad-text-primary-foreground flowglad-shadow-xs hover:flowglad-bg-primary/90',
@@ -53,7 +56,14 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(
+        buttonVariants({
+          variant,
+          size,
+          className,
+          disabled: props.disabled,
+        })
+      )}
       {...props}
     />
   )
