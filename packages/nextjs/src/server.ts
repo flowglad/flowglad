a65dbe68ea08cabfa9179a31324b823ae72ba334
Agree Ahmed <agree.ahmed@gmail.com>
2025-03-29 13:35:08 -0400

    pages route handler, should 'just work'

--- packages/nextjs/src/server.ts
+++ packages/nextjs/src/server.ts
@@ -1,8 +1,8 @@
 // We need to export server modules in a separate file from
 // client modules because otherwise the consumer's next bundler
 // will include client modules in server code
-export { createNextRouteHandler } from './createNextRouteHandler'
-
+export { createAppRouterRouteHandler } from './createAppRouterRouteHandler'
+export { createPagesRouterRouteHandler } from './createPagesRouterRouteHandler'
 export {
   FlowgladServer,
   FlowgladServerAdmin,
