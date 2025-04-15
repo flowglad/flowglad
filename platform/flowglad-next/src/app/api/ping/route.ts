d6d778af98bde596ef2235faaa829a3b16de53f1
Agree Ahmed <agree.ahmed@gmail.com>
2025-04-15 16:22:32 -0400

    fix route

--- platform/flowglad-next/src/app/api/ping/route.ts
+++ platform/flowglad-next/src/app/api/ping/route.ts
@@ -1,14 +1,10 @@
-import { pricesClientSelectSchema } from '@/db/schema/prices'
-import { getStripeOAuthUrl } from '@/utils/stripe'
 import { NextResponse } from 'next/server'
-import SuperJSON from 'superjson'
 
 export const dynamic = 'force-dynamic'
 
 export const GET = async () => {
   return NextResponse.json({
     message: 'pong',
-    oauthURL: getStripeOAuthUrl(),
     gitCommit: process.env.VERCEL_GIT_COMMIT_SHA || 'unknown',
     gitBranch: process.env.VERCEL_GIT_COMMIT_REF || 'unknown',
   })
