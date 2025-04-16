10e396467b396581e903be8590763b2e91d2d525
Agree Ahmed <agree.ahmed@gmail.com>
2025-04-15 22:10:03 -0400

    polyfill crypto

--- platform/flowglad-next/vitest.setup.ts
+++ platform/flowglad-next/vitest.setup.ts
@@ -1,8 +1,15 @@
 // vitest.setup.ts
+import { webcrypto } from 'node:crypto'
 import { stripeServer } from './mocks/stripeServer'
 import { triggerServer } from './mocks/triggerServer'
 import { beforeAll, afterAll, afterEach } from 'vitest'
 import { seedDatabase } from './seedDatabase'
+
+// Polyfill crypto for Node.js environment
+// needed for github actions
+if (!global.crypto) {
+  global.crypto = webcrypto as unknown as Crypto
+}
 
 // Start the mock server before all tests
 beforeAll(async () => {
