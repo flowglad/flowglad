617d4508b00e942086879622b0e1c519c04b2fb1
Agree Ahmed <agreea@users.noreply.github.com>
2025-04-21 10:24:52 -0700

    FlowgladTheme Dark Mode Awareness (#126)

--- packages/react/src/FlowgladTheme.tsx
+++ packages/react/src/FlowgladTheme.tsx
@@ -1,5 +1,5 @@
 // @flowglad/react/src/FlowgladTheme.tsx
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { styles } from './generated/styles'
 
 interface FlowgladThemeProps {
@@ -11,17 +11,42 @@ interface FlowgladThemeProps {
 // Create a stable unique identifier for our styles
 const STYLE_HREF = '@flowglad/react/styles'
 
+const useThemeDetector = () => {
+  const getCurrentTheme = () =>
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme())
+  const mqListener = (e: MediaQueryListEvent) => {
+    setIsDarkTheme(e.matches)
+  }
+
+  useEffect(() => {
+    const darkThemeMq = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    )
+    darkThemeMq.addEventListener('change', mqListener)
+    return () => darkThemeMq.removeEventListener('change', mqListener)
+  }, [])
+  return isDarkTheme
+}
+
 export const FlowgladTheme: React.FC<FlowgladThemeProps> = ({
   children,
-  darkMode = false,
+  darkMode,
   nonce,
 }) => {
-  const rootClassName = `flowglad-root${darkMode ? ' flowglad-dark' : ''}`
-
+  const isDarkTheme = useThemeDetector()
   useEffect(() => {
     // Apply the class to the html element
     document.documentElement.classList.add('flowglad-root')
-    if (darkMode) {
+    // If darkMode is provided, apply the class to the html element
+    // If darkMode is not provided, use the system theme
+    if (typeof darkMode === 'boolean') {
+      if (darkMode) {
+        document.documentElement.classList.add('flowglad-dark')
+      } else {
+        document.documentElement.classList.remove('flowglad-dark')
+      }
+    } else if (isDarkTheme) {
       document.documentElement.classList.add('flowglad-dark')
     }
 
