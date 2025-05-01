47fdb0e453288008fb7912ee9af815bfbb4a976b
Agree Ahmed <agree.ahmed@gmail.com>
2025-05-01 14:41:29 -0400

    core route

--- platform/hosted-billing/src/utils/core.ts
+++ platform/hosted-billing/src/utils/core.ts
@@ -3,16 +3,8 @@ export const IS_DEV = process.env.VERCEL_ENV === 'development'
 export const portalRoute = (params: {
   organizationId: string
   customerExternalId: string
-  page: 'sign-in' | 'manage' | 'validate-magic-link'
+  page: 'sign-in' | 'manage'
 }) => {
-  console.log('portalRoute', {
-    organizationId: params.organizationId,
-    customerExternalId: params.customerExternalId,
-    page: params.page,
-  })
   const routeResult = `/p/${params.organizationId}/${params.customerExternalId}/${params.page}`
-  console.log('routeResult', {
-    routeResult,
-  })
   return routeResult
 }
