a3c004d832108475ead3c8934e20e55782adcae6
Agree Ahmed <agreea@users.noreply.github.com>
2025-05-11 15:34:37 -0700

    Fix Darkmode Styling (#193)
    ## What Does this PR Do?
    - Fix dark mode styling

--- platform/flowglad-next/src/components/forms/MultiSelect.tsx
+++ platform/flowglad-next/src/components/forms/MultiSelect.tsx
@@ -12,7 +12,7 @@ import {
   CommandItem,
   CommandList,
 } from '@/components/ui/command'
-import { cn } from '@/lib/utils'
+import { cn } from '@/utils/core'
 import Hint from '../ion/Hint'
 
 export interface Option {
--- platform/flowglad-next/src/components/ui/command.tsx
+++ platform/flowglad-next/src/components/ui/command.tsx
@@ -5,7 +5,7 @@ import { type DialogProps } from '@radix-ui/react-dialog'
 import { Command as CommandPrimitive } from 'cmdk'
 import { Search } from 'lucide-react'
 
-import { cn } from '@/lib/utils'
+import { cn } from '@/utils/core'
 import Modal, { ModalContent } from '@/components/ion/Modal'
 
 const Command = React.forwardRef<
