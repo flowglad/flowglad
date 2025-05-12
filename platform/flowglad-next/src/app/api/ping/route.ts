a999058fe9ce0770e9246e959fe09de03282313d
Agree Ahmed <agreea@users.noreply.github.com>
2025-05-12 08:39:58 -0700

    Rm Svix on Ping (#196)
    ## What Does this PR Do?
    - rm unnecessary call to svix from our /ping endpoint

--- platform/flowglad-next/src/app/api/ping/route.ts
+++ platform/flowglad-next/src/app/api/ping/route.ts
@@ -1,5 +1,4 @@
 import { core } from '@/utils/core'
-import { svix } from '@/utils/svix'
 import { NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
@@ -7,7 +6,6 @@ export const dynamic = 'force-dynamic'
 export const GET = async () => {
   return NextResponse.json({
     message: 'pong',
-    someSvix: await svix().application.list(),
     gitCommit: process.env.VERCEL_GIT_COMMIT_SHA || 'unknown',
     gitBranch: process.env.VERCEL_GIT_COMMIT_REF || 'unknown',
     isTest: core.IS_TEST,
