6f956a0e2ed5a5c6344b3b33a265e263ec601d27
Agree Ahmed <agree.ahmed@gmail.com>
2025-06-11 10:00:18 -0400

    consolidate migration

--- platform/flowglad-next/drizzle-migrations/0222_nebulous_banshee.sql
+++ platform/flowglad-next/drizzle-migrations/0222_nebulous_banshee.sql
@@ -5,4 +5,10 @@ ALTER TABLE "subscriptions" ALTER COLUMN "current_billing_period_end" DROP NOT N
 ALTER TABLE "subscriptions" ALTER COLUMN "interval" DROP NOT NULL;--> statement-breakpoint
 ALTER TABLE "subscriptions" ALTER COLUMN "interval_count" DROP NOT NULL;--> statement-breakpoint
 ALTER TABLE "subscriptions" ALTER COLUMN "billing_cycle_anchor_date" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "prices" ADD COLUMN "usage_events_per_unit" integer;
+ALTER TABLE "prices" ADD COLUMN "usage_events_per_unit" integer;--> statement-breakpoint
+ALTER TABLE "prices" ADD COLUMN "overage_price_id" text;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "prices" ADD CONSTRAINT "prices_overage_price_id_prices_id_fk" FOREIGN KEY ("overage_price_id") REFERENCES "public"."prices"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
