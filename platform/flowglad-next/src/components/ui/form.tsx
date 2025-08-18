40cb366fac78d694336f5ba0419b32d3c1bc4f52
Agree Ahmed <agree.ahmed@gmail.com>
2025-08-18 14:53:14 -0400

    discount code stability

--- platform/flowglad-next/src/components/ui/form.tsx
+++ platform/flowglad-next/src/components/ui/form.tsx
@@ -45,9 +45,10 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
-
-  const fieldState = getFieldState(fieldContext.name, formState)
+  console.log('====== useFormContext', useFormContext)
+  const formContext = useFormContext()
+  console.log('====== formContext', formContext)
+  const fieldState = formContext.getFieldState(fieldContext.name)
 
   if (!fieldContext) {
     throw new Error('useFormField should be used within <FormField>')
