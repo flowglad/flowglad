ca8cba607bca3b6747a468f3623f124c63c7f46e
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-09-04 15:43:40 -0400

    remove problamatic .passthrough()

--- platform/flowglad-next/src/db/schema/events.ts
+++ platform/flowglad-next/src/db/schema/events.ts
@@ -107,18 +107,16 @@ export const events = pgTable(
   }
 ).enableRLS()
 
-export const eventPayloadSchema = z
-  .object({
-    id: z.string(),
-    object: core.createSafeZodEnum(EventNoun),
-    customer: z
-      .object({
-        id: z.string(),
-        externalId: z.string(),
-      })
-      .optional(),
-  })
-  .passthrough()
+export const eventPayloadSchema = z.object({
+  id: z.string(),
+  object: core.createSafeZodEnum(EventNoun),
+  customer: z
+    .object({
+      id: z.string(),
+      externalId: z.string(),
+    })
+    .optional(),
+})
 
 const columnRefinements = {
   type: core.createSafeZodEnum(FlowgladEventType),
