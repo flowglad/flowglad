479d7d90352453faae447408e84ad232d302443b
Agree Ahmed <agree.ahmed@gmail.com>
2025-09-15 13:16:37 -0400

    page.tsx

---
+++ platform/hosted-billing/src/app/(webapp)/p/[organizationId]/[externalId]/[..rest]/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from 'next/navigation'
+
+const HomePage = async ({
+  params,
+}: {
+  params: Promise<{ organizationId: string; externalId: string }>
+}) => {
+  const { organizationId } = await params
+  redirect(
+    `${process.env.API_BASE_URL}/billing-portal/${organizationId}`
+  )
+  return <div>Sign In Page</div>
+}
+
+export default HomePage
