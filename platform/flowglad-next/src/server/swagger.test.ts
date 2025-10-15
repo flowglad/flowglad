44ceec5ef4ab8abf1294920526071c0c733063e2
Agree Ahmed <agree.ahmed@gmail.com>
2025-10-15 00:49:40 -0400

    swagger test

--- platform/flowglad-next/src/server/swagger.test.ts
+++ platform/flowglad-next/src/server/swagger.test.ts
@@ -340,7 +340,7 @@ describe('Swagger Configuration', () => {
         const route = paths?.[`${basePath}/{id}`]
         expect(route).toBeDefined()
         expect(Object.keys(route || {}).sort()).toEqual(
-          ['get', 'put'].sort()
+          ['get'].sort()
         )
       })
     })
