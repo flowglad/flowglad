4d757d2008fba2ad26ab70c9e2fa592843486f91
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-10-18 13:59:51 -0400

    rename test to match naming convention

--- platform/flowglad-next/src/subscriptions/billingRunHelpers.test.ts
+++ platform/flowglad-next/src/subscriptions/billingRunHelpers.test.ts
@@ -2218,7 +2218,7 @@ describe('billingRunHelpers', async () => {
     })
   })
 
-  describe('billingPeriodItemsToInvoiceLineItemInserts', () => {
+  describe('billingPeriodItemsAndUsageOveragesToInvoiceLineItemInserts', () => {
     let invoice: Invoice.Record
     beforeEach(async () => {
       invoice = await setupInvoice({
