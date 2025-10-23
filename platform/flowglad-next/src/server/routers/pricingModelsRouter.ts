ffb319c5019ead1dc32de1f63756b9c7ac774fb4
angihe93 <angelhe903@gmail.com>
2025-10-23 12:19:58 -0400

    wip with working yaml download

--- platform/flowglad-next/src/server/routers/pricingModelsRouter.ts
+++ platform/flowglad-next/src/server/routers/pricingModelsRouter.ts
@@ -33,6 +33,8 @@ import {
 import { setupPricingModelSchema } from '@/utils/pricingModels/setupSchemas'
 import { TRPCError } from '@trpc/server'
 import { adminTransaction } from '@/db/adminTransaction'
+import { getPricingModelSetupData } from '@/utils/pricingModels/setupHelpers'
+import yaml from 'json-to-pretty-yaml'
 
 const { openApiMetas, routeConfigs } = generateOpenApiMetas({
   resource: 'pricingModel',
@@ -304,6 +306,30 @@ const setupPricingModelProcedure = protectedProcedure
     )
   )
 
+const exportPricingModelProcedure = protectedProcedure
+  .input(idInputSchema)
+  .output(
+    z.object({
+      pricingModelYAML: z
+        .string()
+        .describe('YAML representation of the pricing model'),
+    })
+  )
+  .query(async ({ input, ctx }) => {
+    return authenticatedTransaction(
+      async ({ transaction }) => {
+        const data = await getPricingModelSetupData(
+          input.id,
+          transaction
+        )
+        return { pricingModelYAML: yaml.stringify(data) }
+      },
+      {
+        apiKey: ctx.apiKey,
+      }
+    )
+  })
+
 export const pricingModelsRouter = router({
   list: listPricingModelsProcedure,
   setup: setupPricingModelProcedure,
@@ -313,4 +339,5 @@ export const pricingModelsRouter = router({
   update: updatePricingModelProcedure,
   clone: clonePricingModelProcedure,
   getTableRows: getTableRowsProcedure,
+  export: exportPricingModelProcedure,
 })
