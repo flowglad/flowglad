91ee2a49aab2cbdac422dd6c5566a20858835334
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-10-24 17:57:17 -0400

    fix: add new files

---
+++ platform/flowglad-next/src/email-templates/organization/payout-notification.tsx
@@ -0,0 +1,40 @@
+import { Img, Section } from '@react-email/components'
+import * as React from 'react'
+import { EmailButton } from '../components/EmailButton'
+import { EmailLayout, Header, Paragraph } from '../components/themed'
+
+interface PayoutNotificationEmailProps {
+  organizationName: string
+}
+
+export const PayoutNotificationEmail = ({ organizationName }: PayoutNotificationEmailProps) => {
+  const contactEmail = `mailto:hello@flowglad.com?subject=Payout Enablement Request&body=Hi Flowglad team,%0A%0AI would like to request payout enablement for my organization: ${organizationName}%0A%0AThank you!`
+  
+  return (
+    <EmailLayout
+      previewText={`Enable payouts for ${organizationName}`}
+      variant="organization"
+    >
+      <Img
+        src={`https://cdn-flowglad.com/flowglad-banner-rounded.png`}
+        width="540"
+        height="199"
+        alt="Flowglad Logo"
+        style={{ margin: '0 auto', marginBottom: '32px' }}
+      />
+      <Header
+        title={`Congratulations! ${organizationName} is fully onboarded`}
+        variant="organization"
+      />
+      <Paragraph variant="organization">
+        Your organization has completed onboarding and is nearly ready to process payments. To enable payouts, we need to manually review your account. Please contact us below to get started.
+      </Paragraph>
+
+      <Section style={{ textAlign: 'center' as const, marginTop: '32px' }}>
+        <EmailButton href={contactEmail}>
+          Contact Flowglad Team
+        </EmailButton>
+      </Section>
+    </EmailLayout>
+  )
+}
