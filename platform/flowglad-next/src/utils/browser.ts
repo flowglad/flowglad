1f1d12dc8fb356fa5f82722af5cbce4930251a59
Agree Ahmed <agree.ahmed@gmail.com>
2025-11-06 15:33:12 -0500

    browser list

--- platform/flowglad-next/src/utils/browser.ts
+++ platform/flowglad-next/src/utils/browser.ts
@@ -1,8 +1,15 @@
 export const initBrowser = async () => {
   if (typeof window !== 'undefined') {
-    throw new Error('initBrowser must only be invoked in a server context')
+    throw new Error(
+      'initBrowser must only be invoked in a server context'
+    )
   }
-
+  /**
+   * puppeteer needs to be dynamically imported so that Next.js doesn't try to bundle it
+   * as part of the dependency graph for trigger.dev tasks.
+   *
+   * Since dynamic import is supported by Node as of version ≥18, we can use it here.
+   */
   const [{ default: puppeteer }] = await Promise.all([
     import('puppeteer'),
     // Force Next.js output tracing to include ws when bundling with Bun.
