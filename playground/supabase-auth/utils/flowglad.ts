d82b3a1e98b633465e75473cf8c81b95d01003f4
Agree Ahmed <agree.ahmed@gmail.com>
2025-11-12 11:34:46 -0500

    fix supabase auth

--- playground/supabase-auth/utils/flowglad.ts
+++ playground/supabase-auth/utils/flowglad.ts
@@ -5,12 +5,12 @@ export const flowgladServer = new FlowgladServer({
   supabaseAuth: {
     client: createClient
   },
-  getRequestingCustomer: async () => {
-    return {
-      email: 'test_lklkajsdlfkjasdfjalsfdjasdf@test.com',
-      name: 'Test User',
-      externalId: '___IPasdkfasdfasdfasdfjalks123'
-    };
-  },
+  // getRequestingCustomer: async () => {
+  //   return {
+  //     email: 'test_lklkajsdlfkjasdfjalsfdjasdf@test.com',
+  //     name: 'Test User',
+  //     externalId: '___IPasdkfasdfasdfasdfjalks123'
+  //   };
+  // },
   baseURL: 'http://localhost:3000'
 });
