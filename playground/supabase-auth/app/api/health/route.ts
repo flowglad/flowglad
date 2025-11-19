6f709d0c7af0a353162254bb03098feb0e5de6db
Agree Ahmed <agree.ahmed@gmail.com>
2025-11-19 12:12:40 -0500

    health route

--- playground/supabase-auth/app/api/health/route.ts
+++ playground/supabase-auth/app/api/health/route.ts
@@ -1,18 +1,3 @@
-import { FlowgladServer } from '@flowglad/nextjs/server';
-import { createClient } from '@/utils/supabase/server';
-import { NextResponse } from 'next/server';
-
 export const GET = async (req: Request) => {
-  const supabase = await createClient();
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
-  const flowgladServer = new FlowgladServer({
-    customerExternalId: user?.id,
-    baseURL: 'http://localhost:3000'
-  });
-  const billing = await flowgladServer.getBilling();
-  return NextResponse.json({
-    billing
-  });
+  return new Response('OK', { status: 200 });
 };
