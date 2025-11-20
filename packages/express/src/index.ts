052f5d88393a28bf3938bd19dab833650bf37a42
angihe93 <76918646+angihe93@users.noreply.github.com>
2025-11-20 13:08:20 -0500

    Update packages/express/src/index.ts

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,7 +1,3 @@
-export { FlowgladServer } from '@flowglad/server'
-export {
-  verifyWebhook,
-  WebhookVerificationError,
-} from '@flowglad/server'
+export { FlowgladServer, verifyWebhook, WebhookVerificationError } from '@flowglad/server'
 export { createFlowgladExpressRouter } from './createFlowgladExpressRouter'
 export { createExpressRouteHandler } from './createFlowgladExpressRouteHandler'
