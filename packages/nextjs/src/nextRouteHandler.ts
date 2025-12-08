cb9ac0b07586140a731a4654d380b0a0ed60bcd2
angihe93 <76918646+angihe93@users.noreply.github.com>
2025-12-08 18:05:44 -0500

    uncancel in example project

--- packages/nextjs/src/nextRouteHandler.ts
+++ packages/nextjs/src/nextRouteHandler.ts
@@ -151,7 +151,10 @@ export const nextRouteHandler = (
           req.method === 'GET'
             ? Object.fromEntries(req.nextUrl.searchParams)
             : undefined,
-        body: req.method !== 'GET' ? await req.json() : undefined,
+        body:
+          req.method !== 'GET'
+            ? await req.json().catch(() => ({}))
+            : undefined,
       })
 
       return NextResponse.json(
