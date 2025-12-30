088fc50438b93fa32b64a534e3a18d6222ec3c67
Agree Ahmed <agree.ahmed@gmail.com>
2025-12-30 15:41:52 -0500

    Unify org creation fields

--- platform/flowglad-next/src/app/onboarding/business-details/page.tsx
+++ platform/flowglad-next/src/app/onboarding/business-details/page.tsx
@@ -5,34 +5,16 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { trpc } from '@/app/_trpc/client'
 import ErrorLabel from '@/components/ErrorLabel'
-import OrganizationOnboardingFormFields from '@/components/forms/OrganizationOnboardingFormFields'
+import OrganizationFormFields from '@/components/forms/OrganizationFormFields'
 import { Button } from '@/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormItem,
-  FormLabel,
-} from '@/components/ui/form'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
+import { Form } from '@/components/ui/form'
 import { useAuthContext } from '@/contexts/authContext'
 import {
   type CreateOrganizationInput,
   createOrganizationSchema,
 } from '@/db/schema/organizations'
-import {
-  REFERRAL_OPTIONS,
-  type ReferralOption,
-} from '@/utils/referrals'
-
-const isReferralOption = (value: string): value is ReferralOption => {
-  return REFERRAL_OPTIONS.some((option) => option === value)
-}
+import core from '@/utils/core'
+import { type ReferralOption } from '@/utils/referrals'
 
 const errorMessage = (error: unknown): string => {
   if (error instanceof Error) {
@@ -48,8 +30,6 @@ const BusinessDetails = () => {
   const createOrganization = trpc.organizations.create.useMutation()
   const setReferralSelection =
     trpc.utils.setReferralSelection.useMutation()
-  const { data: countriesList, isLoading: countriesLoading } =
-    trpc.countries.list.useQuery()
   const { setOrganization } = useAuthContext()
   const [referralSource, setReferralSource] = useState<
     ReferralOption | undefined
@@ -72,7 +52,10 @@ const BusinessDetails = () => {
   )
   const onSubmit = form.handleSubmit(async (data) => {
     try {
-      if (!data.organization.stripeConnectContractType) {
+      if (
+        !core.IS_PROD &&
+        !data.organization.stripeConnectContractType
+      ) {
         form.setError('organization.stripeConnectContractType', {
           message: 'Select a payment processing option.',
         })
@@ -110,46 +93,20 @@ const BusinessDetails = () => {
               className="w-[380px] flex flex-col gap-6"
             >
               {/* FIXME (FG-555): Readd OrganizationLogoInput to this page once we have a way to upload the logo during organization creation */}
-              <OrganizationOnboardingFormFields
-                countries={countriesList?.countries ?? []}
+              <OrganizationFormFields
+                setReferralSource={setReferralSource}
+                referralSource={referralSource}
               />
-              <FormItem>
-                <FormLabel>How did you hear about us?</FormLabel>
-                <FormControl>
-                  <Select
-                    value={referralSource}
-                    onValueChange={(val: string) => {
-                      if (isReferralOption(val)) {
-                        setReferralSource(val)
-                        return
-                      }
-                      setReferralSource(undefined)
-                    }}
-                    disabled={countriesLoading}
-                  >
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select an option" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {REFERRAL_OPTIONS.map((option) => (
-                        <SelectItem key={option} value={option}>
-                          {option}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-              </FormItem>
               <Button
                 variant="default"
                 size="default"
                 type="submit"
                 disabled={
                   form.formState.isSubmitting ||
-                  countriesLoading ||
                   !referralSource ||
                   !selectedCountryId ||
-                  !selectedStripeConnectContractType
+                  (!core.IS_PROD &&
+                    !selectedStripeConnectContractType)
                 }
                 className="w-full"
               >
