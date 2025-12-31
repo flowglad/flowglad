d7724033228ce1594b939ec02d9a0098d079f595
Rebecca Xiong <rebeccaxiong111@gmail.com>
2025-12-31 17:52:34 -0500

    update entry point to add session check

--- platform/flowglad-next/src/app/billing-portal/[organizationId]/[customerId]/page.tsx
+++ platform/flowglad-next/src/app/billing-portal/[organizationId]/[customerId]/page.tsx
@@ -1,5 +1,22 @@
+import { redirect } from 'next/navigation'
+import { getSession } from '@/utils/auth'
 import InternalBillingPortalPage from './Internal'
 
-export default async function BillingPortalPage() {
+interface BillingPortalRedirectPageProps {
+  params: Promise<{
+    organizationId: string
+    customerId: string
+  }>
+}
+export default async ({ params }: BillingPortalRedirectPageProps) => {
+  const { organizationId, customerId } = await params
+  const session = await getSession()
+
+  if (!session) {
+    redirect(
+      `/billing-portal/${organizationId}/${customerId}/sign-in`
+    )
+  }
+
   return <InternalBillingPortalPage />
 }
